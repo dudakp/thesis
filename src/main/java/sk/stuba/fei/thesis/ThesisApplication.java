4d1d1c16cc8f0da9de520ce982541996f1d09fba
dudakp <M2jt6vSDS>
2019-10-17 19:43:22 +0200

    Initial Commit

---
+++ src/main/java/sk/stuba/fei/thesis/ThesisApplication.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.thesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThesisApplication.class, args);
+    }
+
+}
